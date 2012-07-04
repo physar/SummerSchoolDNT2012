cbcc28d6583efef0f5996f5eed552b145a982a67
Auke Wiggers <wiggers.auke@gmail.com>
2012-07-04 16:16:03 +0200

    Finished motion_update

--- modules/day4/localization_v1.py
+++ modules/day4/localization_v1.py
@@ -1,5 +1,8 @@
 from math import cos, sin, pi
 
+
+import mazeParser
+
 class localization_v1():    
     globals = None
 
@@ -7,123 +10,210 @@ class localization_v1():
     #    self.globals = modules.getModule("globals")    
         
     def localize_in_maze(self):
+        """
+        INPUT: None
+        OUTPUT: Position (x,y,z)
+
+        Main loop, finds the current location of the nao in the maze
+        """
+
+        edges, qrpos, valid_moves = mazeParser.parseMaze()
+        for move in valid_moves:
+            print move, valid_moves[move]
+        
         # initialize belief in the map (4x4 positions, 4 orientations)
         b = 1/ ( 4.0**3 )
+        map_belief = [[[b for i in range(4)] for i in range(4)] for i in range(4)]
 
-        edges = {(1, 3): [(0, 3)], (3, 0): [(2, 0), (4, 0)],
-                 (2, 1): [(3, 1)], (0, 3): [(0, 2), (1, 3), (0, 4)],
-                 (4, 0): [(3, 0), (4, 1)], (1, 2): [(0, 2), (2, 2)],
-                 (3, 3): [(2, 3), (4, 3)], (4, 4): [(4, 3), (3, 4)],
-                 (0, 4): [(0, 3), (1, 4)], (4, 1): [(4, 0), (3, 1), (4, 2)],
-                 (1, 1): [(0, 1)], (3, 2): [(4, 2)], (0, 0): [(1, 0), (0, 1)],
-                 (2, 2): [(1, 2)], (1, 4): [(0, 4), (2, 4)], (2, 3): [(3, 3)],
-                 (4, 2): [(4, 1), (3, 2), (4, 3)], (1, 0): [(0, 0), (2, 0)],
-                 (0, 1): [(0, 0), (1, 1), (0, 2)], (3, 1): [(2, 1), (4, 1)],
-                 (2, 4): [(1, 4), (3, 4)], (2, 0): [(1, 0), (3, 0)],
-                 (4, 3): [(4, 2), (3, 3), (4, 4)], (3, 4): [(2, 4), (4, 4)],
-                 (0, 2): [(0, 1), (1, 2), (0, 3)]}
+        state = 1,0,0
+        running = 1
+        while running:
+            print "Current REAL state", state
+            
+            # Ask user for a new motion
+            motion = 0,0,0
+            try:
+                motion = eval(raw_input("Input a motion in the form (x,y,theta), in naocoordinates: "))
+                if type(motion) != tuple:
+                    raise Error, "Fiets"
+            except:
+                print "Motion must be of type tuple: (x,y,theta)"
+                continue
+
+            # if the move is possible, execute it
+            x, y, t = state
+            if self.is_valid_move(state, motion, valid_moves):
+                dx = int(cos(t)) * motion[0]
+                dy = int(sin(t)) * motion[1]
+                dt = motion[2]
+                newstate = x+dx, y+dy, t+dt
+                state = newstate
+                self.walk(dx, dy, dt)
+                
+                # update the belief
+                motion_map_belief = self.motion_update(motion, map_belief, edges, valid_moves)
+
+                observation = self.perceive()
+                # if an observation is made, use it to update the belief
+                if observation:
+                    map_belief = self.vision_update(observation, motion_map_belief, valid_moves)
+                else:
+                    map_belief = motion_map_belief
+            else:
+                print "That move is not valid!"
+                
+            running = False
+
+    def walk(self, x, y, theta):
+        """
+        INPUT: x, y, theta
+        RETURNS: None
+
+        Walk function that takes x,y,t and walks accordingly
+        (x,y,t are in grid coordinates!)
+        """
+        pass
         
-        map_belief = [[[b,b,b,b],
-                       [b,b,b,b],
-                       [b,b,b,b],
-                       [b,b,b,b]],
-                      [[b,b,b,b],
-                       [b,b,b,b],
-                       [b,b,b,b],
-                       [b,b,b,b]],
-                      [[b,b,b,b],
-                       [b,b,b,b],
-                       [b,b,b,b],
-                       [b,b,b,b]],
-                      [[b,b,b,b],
-                       [b,b,b,b],
-                       [b,b,b,b],
-                       [b,b,b,b]]]
-        try:
-            motion = eval(raw_input("Input a motion in the form (x,y,theta): "))
-            if type(motion) != tuple:
-                raise Error, "Fiets"
-        except:
-            print "Motion must be of type tuple: (x,y,theta)"
-            return False
+    def perceive(self):
+        """
+        RETURNS: bearing, distance, signature or None
 
-        self.motion_update([0.0,0.0,0.0], motion, map_belief, edges)
-
+        If a blob is seen, return its information, else None
+        """
+        return (0,1,0)
         
-    def motion_update(self, state, motion, map_belief, map_edges):
+    def motion_update(self, motion, map_belief, map_edges, valid_moves):
         """
         INPUT: state, motion, map_belief, map_edges
-        RETURN: new state , new map_belief
-
+        RETURNS: new state , new map_belief
         """
         # motion update vector u
         u_x, u_y, u_t = motion
         
-        # update the current state, if possible
-        
-        pOvershoot = 0.1
-        pUndershoot = 0.1
-        pExact = 0.8
         # Initiate new map_belief for every position and orientation
         new_map_belief = [[[0 for i in range(4)] for i in range(4)] for i in range(4)]
 
         # Create the new belief over possible positions in the map
         for col in range(4):
             for row in range(4):
-                for orientation in range(4):                   
-                    old_x = row - u_x
-                    old_y = col - u_y
+                for orientation in range(4):                
+                    
+                    # keep orientation in mind!
+                    t = orientation * pi / 2.0
+                    u_x_rot = int(cos(t)*u_x - sin(t) * u_y)
+                    u_y_rot = int(sin(t)*u_x + cos(t) * u_y)
+                            
+                    old_x = col - u_x_rot
+                    old_y = row - u_y_rot
                     # orientation = 3 + move_rotation = 1 --> new orientation = 0
                     old_t = orientation - u_t % 4
+
+                    old_location = (old_x, old_y, old_t)
                     
-                    if self.is_valid_move((old_x, old_y, old_t), motion, map_edges):
+                    if self.is_valid_move(old_location, [u_x_rot,u_y_rot,u_t], valid_moves):
                         # belief is transferred from old positions to the new ones
                         belief = map_belief[old_x][old_y][old_t]
-                        new_map_belief[row][col][orientation] = belief
+                        new_map_belief[col][row][orientation] += belief
+                        
                     else:
                         # if, based on the move, it is impossible to be here,
                         # the new probability is 0
-                        new_map_belief[row][col][orientation] = 0
+                        new_map_belief[col][row][orientation] += 0
                         
         # normalize the array map (evenly spread out probabilities)
         new_map_belief = self.normalize_3D(new_map_belief)
         # print it in a fashionable way!
         self.prettyPrint(new_map_belief)
+        return new_map_belief
 
-    def is_valid_move(self, position, motion, map_edges):
+    def is_valid_move(self, position, motion, valid_moves):
         """
-        INPUT: position, motion, map_edges
+        INPUT: position, motion, valid_moves
         RETURNS: True/False
 
         Is the move valid?
         """
         # the position contains x, y and orientation
         x, y, t = position
-        u_unrotated, v_unrotated, w = motion
-        # keep orientation in mind!
-        t = t * pi / 2.0
-        u = int(cos(t)*u_unrotated - sin(t) * v_unrotated)
-        v = int(sin(t)*u_unrotated + cos(t) * v_unrotated)
+        u, v, w = motion
+        
         # moves must end IN the maze
-        if not(0 < x + u < 4 and 0 < y + v < 4):
+        if not(0 <= x + u < 4 and 0 <= y + v < 4):
             return False
         # and there must not be edges in the way
-        validMoves = {(0, 1): [(1, 1)], (1, 2): [(0, 2), (2, 2), (1, 3)], (3, 2): [(2, 2)], (0, 0): [(1, 0)], (3, 3): [(2, 3)], (3, 0): [(2, 0)], (3, 1): [(2, 1)], (2, 1): [(1, 1), (3, 1), (2, 2)], (0, 2): [(1, 2)], (2, 0): [(1, 0), (3, 0)], (1, 3): [(1, 2), (0, 3), (2, 3)], (2, 3): [(1, 3), (3, 3)], (2, 2): [(2, 1), (1, 2), (3, 2)], (1, 0): [(0, 0), (2, 0), (1, 1)], (0, 3): [(1, 3)], (1, 1): [(1, 0), (0, 1), (2, 1)]}
-        
-        if not((x+u, y+v) in validMoves[(x,y)] ):
+        if not((x, y) in valid_moves[(x+u,y+v)] ):
             return False
         # if all of the above was evaded, the move is valid
-        print "Move", x,y,"{0:.2f} to".format(t), x+u,y+v, "is valid"
+        #print "Move", x,y,"{0:.2f} to".format(t), x+u,y+v, "is valid"
         return True
     
     #update pose of robot (grid localization)
-    def vision_update(self, state, observation, map_belief):
+    def vision_update(self, observation, map_belief, valid_moves):
         """
-        INPUT: state, observation, map_edges
-        RETURN: state of robot [x,y,theta]
+        INPUT: observation, map_edges
+        RETURNS: map_belief
 
         """    
-        pass
+        distance = self.parseObservation(observation)
+        for col in range(4):
+            for row in range(4):
+                for orientation in range(4):
+                    location = (col, row, orientation)
+                    if not self.is_valid_perception(location, distance, valid_moves):
+                        map_belief[col][row][orientation] = 0    
+        # normalize it (spread out frequencies evenly)
+        new_map_belief = self.normalize_3D(map_belief)
+        return new_map_belief
+    
+    def is_valid_perception(self, location, distance, valid_moves):
+        """
+        INPUT: location, distance, valid_moves (dict)
+        RETURNS: True/False
+
+        Checks if an observation is possible from the current location,
+        given the edges
+        """
+
+        # a distance must be positive
+        if distance < 0:
+            return False
+        
+        x, y, t = location
+
+        # an observation is possible if the moves needed to reach
+        # the corresponding cell are valid
+        if t == 0:
+            for x_wall in range(x, x+distance):
+                if not (x+1, y) in valid_moves[(x,y)]:
+                    return False
+        elif t == 1:
+            for x_wall in range(y, y+distance):
+                if not (x, y+1) in valid_moves[(x,y)]:
+                    return False
+        elif t == 2:
+            for x_wall in range(x-distance, x):
+                if not (x+1, y) in valid_moves[(x,y)]:
+                    return False
+        elif t == 3:
+            for x_wall in range(y-distance, y):
+                if not (x, y+1) in valid_moves[(x,y)]:
+                    return False
+        else:
+            return False
+        return True
+    
+    def parseObservation(self, observation):
+        """
+        INPUT: Observation (bearing, distance, signature)
+        RETURNS: Distance measure in worldcoordinates (cells)
+        """
+        bearing, distance, signature = observation
+        if distance < 0.88:
+            return 0
+        elif distance < 1.76:
+            return 1
+        else:
+            return 2
 
     def prettyPrint(self, map_belief):
         """
@@ -131,9 +221,14 @@ class localization_v1():
 
         Prints the map in a readable format
         """
-        for x in range(len(map_belief)):
-            for y in range(len(map_belief[0])):
-                print x, y, "", map_belief[x][y]
+        print "\t\t\t{0}\t{1}\t{2}\t{3}\t".format(0, 1.57, 3.14, 4.71)
+        print "----------------------------------------------------"
+        for y in range(len(map_belief[0])):
+            for x in range(len(map_belief)):
+                print "Row {0}, col {1} -->\t".format(y,x),
+                for o in range(len(map_belief[0][0])):
+                    print "{0:.2f}\t".format(map_belief[x][y][o]),
+                print ""
 
     def normalize_3D(self, map_belief):
         """
