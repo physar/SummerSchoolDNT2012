04a3b7b981830ab08be94d9f0cb1af5415ade8f9
HesselM <hmolen.science@gmail.com>
2012-07-09 21:13:13 +0200

    error fix

--- assignments/dag2_startcode/modules/globals.py
+++ assignments/dag2_startcode/modules/globals.py
@@ -11,4 +11,4 @@ class globals:
         #self.speechProxy = ALProxy("ALTextToSpeech", self.ipadress, 9559)
         self.motProxy = ALProxy("ALMotion", self.ipadress, 9559)
         self.posProxy = ALProxy("ALRobotPose", self.ipadress, 9559)
-        self.vidProxy = ALProxy( "ALVideoDevice", self.ipadress, 9559 )
+        self.vidProxy = ALProxy("ALVideoDevice", self.ipadress, 9559)
