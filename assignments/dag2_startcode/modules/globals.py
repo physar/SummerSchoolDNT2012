17e8154bdab2e65f4c0f54e89b164d601d69632d
HesselM <hmolen.science@gmail.com>
2012-07-09 21:12:20 +0200

    runnable day2-startcode (without vision functions)

--- assignments/dag2_startcode/modules/globals.py
+++ assignments/dag2_startcode/modules/globals.py
@@ -1,13 +1,14 @@
 from naoqi import ALProxy
 
 class globals:
-    ipadress = "..."
+    ipadress = "localhost"
     
     def setDependencies(self, modules):
         pass
 
     def setProxies(self):
-        self.speechProxy = ALProxy("ALTextToSpeech", self.ipadress, 9559)
+        #not necessary.. 
+        #self.speechProxy = ALProxy("ALTextToSpeech", self.ipadress, 9559)
         self.motProxy = ALProxy("ALMotion", self.ipadress, 9559)
         self.posProxy = ALProxy("ALRobotPose", self.ipadress, 9559)
         self.vidProxy = ALProxy( "ALVideoDevice", self.ipadress, 9559 )
