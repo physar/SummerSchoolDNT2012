e5b1e5d298476487a4c989895f754d298827f4e9
HesselM <hmolen.science@gmail.com>
2012-07-11 11:31:24 +0200

    dag3, startcode deel 1: update hard

---
+++ assignments/dag3_v2_deel1_start/2_HurtMePlenty/modules/pathPlanning/aStar_v1.py
@@ -0,0 +1,9 @@
+import math
+
+class aStar_v1:
+    def setDependencies(self, modules):
+        pass
+    
+    #check wikipedia for pseudo-code:
+    #http://en.wikipedia.org/wiki/A*_search_algorithm
+    #Good luck!
