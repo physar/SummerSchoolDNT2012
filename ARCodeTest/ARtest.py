96e4453e06466c6940e8eb0ba2865d73b6f56eaf
TiRune <TiRune@gmail.com>
2012-07-11 16:22:51 +0200

    fixed some typeos in dag_3 assignment

--- ARCodeTest/ARtest.py
+++ ARCodeTest/ARtest.py
@@ -1,18 +1,16 @@
-import cv #import this first!
-import cv2
-import numpy as np
-import ARimport as ar
+import cv #import opencv met deze twee functies
+import cv2 #
+import numpy as np #import de numpy library
+import ARimport as ar #import de AR code herkennings module
 
 
-ARcode = ar.ARimport()
-#ARcode.test()
-image = cv.LoadImage("test.png",cv.CV_LOAD_IMAGE_COLOR)
-im_array = np.asarray( image[:,:] )
-#im_array = im_array.astype(np.uint32)
-ARcode.setOutputFileName("output.png")
-ARcode.setShowOutput(True)
-returnint = ARcode.findMarkers(im_array)
+ARcode = ar.ARimport()  #maak een class aan van de AR code herkennings module
+image = cv.LoadImage("test.png",cv.CV_LOAD_IMAGE_COLOR) # Laad het test.png plaatje uit de folder (laad deze als kleuren plaatje!)
+im_array = np.asarray( image[:,:] ) #van dit plaatje maken we een numpy array. Het is belangrijk dat dit het type uint_8 heeft (unsigned int 8bit)
+ARcode.setOutputFileName("output.png")  #De output van het algoritme krijgt de volgende naam
+ARcode.setShowOutput(True) #We willen dat het algoritme elke keer een plaatje output met een tekening van de marker
+returnint = ARcode.findMarkers(im_array)  #Zoek de markers, en return een integer met hoeveel markers er gevonden zijn
 
-output = ARcode.getFoundMarker(0) #input < returnint
+output = ARcode.getFoundMarker(0) #input < returnint! output is een struct met .ID .x en .y informatie
 print((output.ID, output.x, output.y))
                     
